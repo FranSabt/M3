fd2ca35869d1e36d17df102dde9001eea9c1e444
FranSabt <hsabatino1@gmail.com>
2022-10-07 19:53:02 -0400

    done

--- 03-WebServer/homework/Beatles/app.js
+++ 03-WebServer/homework/Beatles/app.js
@@ -22,3 +22,51 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+const server = http.createServer((req, res) => {
+  const url = req.url
+
+  if(url === '/'){
+    res.writeHead(200, { 'Content-Type':'text/html' });
+		let html = fs.readFileSync(__dirname +'/html/index.html', 'utf8');
+		return res.end(html);
+  }
+
+  if(url === '/api'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(JSON.stringify(beatles))
+  }
+
+  else {
+    let nUrl = url.toLowerCase();
+    let beatle = beatles.find(e => {
+      let name = e.name.toLowerCase().split(' ')
+      if(('/'+ name[0]) === nUrl || ('/'+ name[1]) === nUrl) return e
+      else return false;
+      
+    })
+
+    console.log(beatle)
+
+  if (beatle) {
+    let html = fs.readFileSync(__dirname +'/html/beatle.html', 'utf8')
+    let beatleName = beatle.name;
+    let beatleBirthdate = beatle.birthdate;
+    let beatleImg = beatle.profilePic;
+    
+
+    html = html.replace('{nombre}', beatleName );
+    html = html.replace('{birthdate}', beatleBirthdate );
+    html = html.replace('{image}', beatleImg);
+    res.end(html)
+  }
+  if (!beatle){
+    res.writeHead(404, { 'Content-Type':'text/plain' })
+    res.end('Beatle not found...  :c')
+  }
+
+  }
+
+})
+
+server.listen(3000, () => console.log('listening on port 3000'));
